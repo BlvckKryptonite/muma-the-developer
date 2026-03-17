b75318199e53e35bdeefc1ad450b59c5d0038be8
BlvckKryptonite <mumathedeveloper@gmail.com>
2026-03-17 09:17:59 +0200

    Comment game xit out.

--- client/src/pages/portfolio.tsx
+++ client/src/pages/portfolio.tsx
@@ -144,15 +144,15 @@ const projects = [
 //       "https://github.com/BlvckKryptonite/ParsePilot/blob/main/README.md",
 //     icon: "/icons/parsepilot.png",
 //   },
-  {
-    name: "Game-Xit",
-    description:
-      "A three-page HTML/CSS site built to raise awareness about video game addiction. Includes a responsive layout, embedded YouTube resources, a gaming self-assessment tool link, and a styled contact form with Google Maps integration.",
-    link: "https://blvckkryptonite.github.io/game-xit/",
-    documentation:
-      "https://github.com/BlvckKryptonite/game-xit/blob/main/README.md",
-    icon: "/icons/gameexit.png",
-  },
+  // {
+  //   name: "Game-Xit",
+  //   description:
+  //     "A three-page HTML/CSS site built to raise awareness about video game addiction. Includes a responsive layout, embedded YouTube resources, a gaming self-assessment tool link, and a styled contact form with Google Maps integration.",
+  //   link: "https://blvckkryptonite.github.io/game-xit/",
+  //   documentation:
+  //     "https://github.com/BlvckKryptonite/game-xit/blob/main/README.md",
+  //   icon: "/icons/gameexit.png",
+  // },
 //   {
 //     name: "FraudShield CLI",
 //     description:
